f0a9d200857bac411e110fc971eba2b679cdc678
December G <dgarnetsmith@gmail.com>
2023-09-29 04:12:34 -0500

    Attempt to add support for other countries

--- scripts/background.js
+++ scripts/background.js
@@ -1,89 +1,177 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   const tabUrl = tab.url ?? tab.pendingUrl;
+  console.log(changeInfo, "changeinfo");
+  console.log("tab", tab);
+  console.log("tab.url", tab.url);
+  const domains = [
+    "airbnb.com",
+    "airbnb.es",
+    "airbnb.ae",
+    "airbnb.al ",
+    "airbnb.am ",
+    "airbnb.at",
+    "airbnb.az",
+    "airbnb.ba ",
+    "airbnb.be ",
+    "airbnb.ca",
+    "airbnb.cat",
+    "airbnb.ch",
+    "airbnb.cl",
+    "airbnb.cn",
+    "airbnb.co.cr",
+    "airbnb.co.id",
+    "airbnb.co.il",
+    "airbnb.co.in",
+    "airbnb.co.kr",
+    "airbnb.com.ar",
+    "airbnb.com.au",
+    "airbnb.com.bo ",
+    "airbnb.com.br",
+    "airbnb.com.bz",
+    "airbnb.com.co",
+    "airbnb.com.ec",
+    "airbnb.com.ee",
+    "airbnb.com.gt",
+    "airbnb.com.hk",
+    "airbnb.com.hn",
+    "airbnb.com.hr",
+    "airbnb.com.kh",
+    "airbnb.com.mt",
+    "airbnb.com.my",
+    "airbnb.com.ni",
+    "airbnb.com.pa",
+    "airbnb.com.pe",
+    "airbnb.com.ph",
+    "airbnb.com.py",
+    "airbnb.com.ro",
+    "airbnb.com.sg",
+    "airbnb.com.sv",
+    "airbnb.com.tr",
+    "airbnb.com.tw",
+    "airbnb.com.ua",
+    "airbnb.com.vn",
+    "airbnb.co.nz",
+    "airbnb.co.uk",
+    "airbnb.co.ve",
+    "airbnb.co.za",
+    "airbnb.cz",
+    "airbnb.de",
+    "airbnb.dk",
+    "airbnb.fi",
+    "airbnb.fr",
+    "airbnb.gr",
+    "airbnb.hu",
+    "airbnb.ie",
+    "airbnb.is",
+    "airbnb.it",
+    "airbnb.jp",
+    "airbnb.la",
+    "airbnb.lt",
+    "airbnb.lu",
+    "airbnb.lv",
+    "airbnb.me",
+    "airbnb.mx",
+    "airbnb.nl",
+    "airbnb.no",
+    "airbnb.pl",
+    "airbnb.pt ",
+    "airbnb.rs",
+    "airbnb.ru",
+    "airbnb.se",
+    "airbnb.si",
+  ];
+  console.log("tabUrl", tabUrl);
+
+  const includesUrl = domains.some((domain) => tabUrl.includes(domain));
+  console.log("includesurl", includesUrl);
   if (
     changeInfo.status === "complete" &&
     tabUrl &&
-    tabUrl.includes(
-      "airbnb.es" ||
-        "airbnb.ae" ||
-        "airbnb.al " ||
-        "airbnb.am " ||
-        "airbnb.at" ||
-        "airbnb.az" ||
-        "airbnb.ba " ||
-        "airbnb.be " ||
-        "airbnb.ca" ||
-        "airbnb.cat" ||
-        "airbnb.ch" ||
-        "airbnb.cl" ||
-        "airbnb.cn" ||
-        "airbnb.co.cr" ||
-        "airbnb.co.id" ||
-        "airbnb.co.il" ||
-        "airbnb.co.in" ||
-        "airbnb.co.kr" ||
-        "airbnb.com.ar" ||
-        "airbnb.com.au" ||
-        "airbnb.com.bo " ||
-        "airbnb.com.br" ||
-        "airbnb.com.bz" ||
-        "airbnb.com.co" ||
-        "airbnb.com.ec" ||
-        "airbnb.com.ee" ||
-        "airbnb.com.gt" ||
-        "airbnb.com.hk" ||
-        "airbnb.com.hn" ||
-        "airbnb.com.hr" ||
-        "airbnb.com.kh" ||
-        "airbnb.com.mt" ||
-        "airbnb.com.my" ||
-        "airbnb.com.ni" ||
-        "airbnb.com.pa" ||
-        "airbnb.com.pe" ||
-        "airbnb.com.ph" ||
-        "airbnb.com.py" ||
-        "airbnb.com.ro" ||
-        "airbnb.com.sg" ||
-        "airbnb.com.sv" ||
-        "airbnb.com.tr" ||
-        "airbnb.com.tw" ||
-        "airbnb.com.ua" ||
-        "airbnb.com.vn" ||
-        "airbnb.co.nz" ||
-        "airbnb.co.uk" ||
-        "airbnb.co.ve" ||
-        "airbnb.co.za" ||
-        "airbnb.cz" ||
-        "airbnb.de" ||
-        "airbnb.dk" ||
-        "airbnb.fi" ||
-        "airbnb.fr" ||
-        "airbnb.gr" ||
-        "airbnb.hu" ||
-        "airbnb.ie" ||
-        "airbnb.is" ||
-        "airbnb.it" ||
-        "airbnb.jp" ||
-        "airbnb.la" ||
-        "airbnb.lt" ||
-        "airbnb.lu" ||
-        "airbnb.lv" ||
-        "airbnb.me" ||
-        "airbnb.mx" ||
-        "airbnb.nl" ||
-        "airbnb.no" ||
-        "airbnb.pl" ||
-        "airbnb.pt " ||
-        "airbnb.rs" ||
-        "airbnb.ru" ||
-        "airbnb.se" ||
-        "airbnb.si" ||
-        "airbnb.com",
-    )
+    // tabUrl.includes(
+    //   "airbnb.com",
+    //   "airbnb.es",
+    //   "airbnb.ae",
+    //   "airbnb.al ",
+    //   "airbnb.am ",
+    //   "airbnb.at",
+    //   "airbnb.az",
+    //   "airbnb.ba ",
+    //   "airbnb.be ",
+    //   "airbnb.ca",
+    //   "airbnb.cat",
+    //   "airbnb.ch",
+    //   "airbnb.cl",
+    //   "airbnb.cn",
+    //   "airbnb.co.cr",
+    //   "airbnb.co.id",
+    //   "airbnb.co.il",
+    //   "airbnb.co.in",
+    //   "airbnb.co.kr",
+    //   "airbnb.com.ar",
+    //   "airbnb.com.au",
+    //   "airbnb.com.bo ",
+    //   "airbnb.com.br",
+    //   "airbnb.com.bz",
+    //   "airbnb.com.co",
+    //   "airbnb.com.ec",
+    //   "airbnb.com.ee",
+    //   "airbnb.com.gt",
+    //   "airbnb.com.hk",
+    //   "airbnb.com.hn",
+    //   "airbnb.com.hr",
+    //   "airbnb.com.kh",
+    //   "airbnb.com.mt",
+    //   "airbnb.com.my",
+    //   "airbnb.com.ni",
+    //   "airbnb.com.pa",
+    //   "airbnb.com.pe",
+    //   "airbnb.com.ph",
+    //   "airbnb.com.py",
+    //   "airbnb.com.ro",
+    //   "airbnb.com.sg",
+    //   "airbnb.com.sv",
+    //   "airbnb.com.tr",
+    //   "airbnb.com.tw",
+    //   "airbnb.com.ua",
+    //   "airbnb.com.vn",
+    //   "airbnb.co.nz",
+    //   "airbnb.co.uk",
+    //   "airbnb.co.ve",
+    //   "airbnb.co.za",
+    //   "airbnb.cz",
+    //   "airbnb.de",
+    //   "airbnb.dk",
+    //   "airbnb.fi",
+    //   "airbnb.fr",
+    //   "airbnb.gr",
+    //   "airbnb.hu",
+    //   "airbnb.ie",
+    //   "airbnb.is",
+    //   "airbnb.it",
+    //   "airbnb.jp",
+    //   "airbnb.la",
+    //   "airbnb.lt",
+    //   "airbnb.lu",
+    //   "airbnb.lv",
+    //   "airbnb.me",
+    //   "airbnb.mx",
+    //   "airbnb.nl",
+    //   "airbnb.no",
+    //   "airbnb.pl",
+    //   "airbnb.pt ",
+    //   "airbnb.rs",
+    //   "airbnb.ru",
+    //   "airbnb.se",
+    //   "airbnb.si",
+    // )
+    includesUrl
   ) {
+    console.log("on working page");
     chrome.scripting.insertCSS({
       target: { tabId: tabId },
       files: ["css/global.css"],
     });
+  } else {
+    console.log("not a valid airbnb domain", tabUrl);
   }
 });
